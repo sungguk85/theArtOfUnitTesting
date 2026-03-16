fd57ec8e2cb79df12d9ba813cf76a9fea9020856
SungGuk Jung <sunggukjung@Mac.lan>
2026-03-16 00:09:46 -0700

    jest - describe, it

--- ch2-first-test/__tests__/password-verifier0.spec.js
+++ ch2-first-test/__tests__/password-verifier0.spec.js
@@ -1,79 +1,83 @@
-const { verifyPassword } = require('../password-verifier0');
+const { verifyPassword } = require("../password-verifier0");
 
-test('v0: badly named test', () => {
-  // 테스트 준비-함수에 전달할 매개변수를 초기화한다.
-  const fakeRule = (input) => ({ passed: false, reason: 'fake reason' });
+describe("verifyPassword", () => {
+  describe("with a failing rule", () => {
+    it("returns errors", () => {
+      const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
+      // 테스트 준비-함수에 전달할 매개변수를 초기화한다.
 
-  // 함수 실행의 인자로 전달한다.
-  const errors = verifyPassword('any value', [fakeRule]);
+      // 함수 실행의 인자로 전달한다.
+      const errors = verifyPassword("any value", [fakeRule]);
 
-  // 함수의 반환 값을 기대 값과 비교한다.
-  expect(errors[0]).toMatch('fake reason');
+      // 함수의 반환 값을 기대 값과 비교한다.
+      expect(errors[0]).toContain("fake reason");
+    });
+  });
 });
 
-test('v1: the first test', () => {
-  const fakeRule = (input) => ({ passed: false, reason: 'fake reason' });
+test("v1: the first test", () => {
+  const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
 
-  const errors = verifyPassword('any value', [fakeRule]);
-  expect(errors[0]).toMatch('fake reason');
+  const errors = verifyPassword("any value", [fakeRule]);
+  expect(errors[0]).toMatch("fake reason");
 });
 
-test('v1.1 verifyPassword, given a failing rule, returns errors', () => {
-  const fakeRule = (input) => ({ passed: false, reason: 'fake reason' });
+test("v1.1 verifyPassword, given a failing rule, returns errors", () => {
+  const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
 
-  const errors = verifyPassword('any value', [fakeRule]);
+  const errors = verifyPassword("any value", [fakeRule]);
 
-  expect(errors[0]).toContain('fake reason');
+  expect(errors[0]).toContain("fake reason");
 });
 
-describe('v1.2: verifyPassword', () => {
-  test('given a failing rule, returns errors', () => {
+describe("v1.2: verifyPassword", () => {
+  test("given a failing rule, returns errors", () => {
     const fakeRule = (input) => ({ passed: false, reason: input });
 
-    const errors = verifyPassword('any value', [fakeRule]);
+    const errors = verifyPassword("any value", [fakeRule]);
 
-    expect(errors[0]).toContain('any value');
+    expect(errors[0]).toContain("any value");
   });
 });
 
-describe('v1.3: verifyPassword', () => {
-  describe('with a failing rule', () => {
-    test('returns errors', () => {
+describe("v1.3: verifyPassword", () => {
+  describe("with a failing rule", () => {
+    test("returns errors", () => {
       const fakeRule = () => ({
         passed: false,
-        reason: 'fake reason',
+        reason: "fake reason",
       });
-      const errors = verifyPassword('any value', [fakeRule]);
+      const errors = verifyPassword("any value", [fakeRule]);
 
-      expect(errors[0]).toContain('fake reason');
+      expect(errors[0]).toContain("fake reason");
     });
   });
 });
 
-describe('v1.4: verifyPassword', () => {
-  describe('with a failing rule', () => {
+describe("v1.4: verifyPassword", () => {
+  describe("with a failing rule", () => {
     const fakeRule = () => ({
       passed: false,
-      reason: 'fake reason',
+      reason: "fake reason",
     });
-    test('returns errors', () => {
-      const errors = verifyPassword('any value', [fakeRule]);
+    test("returns errors", () => {
+      const errors = verifyPassword("any value", [fakeRule]);
 
-      expect(errors[0]).toContain('fake reason');
+      expect(errors[0]).toContain("fake reason");
     });
   });
 });
 
-describe('v1.5: verifyPassword', () => {
-  describe('with a failing rule', () => {
-    it('returns errors', () => {
+describe("v1.5: verifyPassword", () => {
+  describe("with a failing rule", () => {
+    it("returns errors", () => {
       const fakeRule = () => ({
         passed: false,
-        reason: 'fake reason',
+        reason: "fake reason",
       });
-      const errors = verifyPassword('any value', [fakeRule]);
+      const errors = verifyPassword("any value", [fakeRule]);
 
-      expect(errors[0]).toContain('fake reason');
+      expect(errors[0]).toContain("fake reason");
     });
   });
 });
